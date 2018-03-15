d76766c652f1dc1d1d44edd6d916b51cdf420472
vmarois <vincent.marois@protonmail.com>
2018-03-15 14:51:39 +0100

    Can display 2 pointclouds on same plan. Dummy translation/rotation for now.

--- examples/pc_lake_position.py
+++ examples/pc_lake_position.py
@@ -1,68 +1,78 @@
 import vtk
 import pandas as pd
 import numpy as np
+import os
+import sys
+
+examples_dir = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(examples_dir, '..', 'python'))
 from depthmotionnet.vis import *
 
+
 """
+** INITIAL TEST **
 - Assume pointclouds have already been exported to CSV with export_pointcloud_to_csv()
 - Assume pointclouds have dropped points towards infinity using find_depth_threshold() & ignore_points_depth_threshold() 
 """
 
 # initial step: load back pointclouds from .csv files
-scene_1 = pd.read_csv("/path/to/csv/scene_1.csv", index_col=0)
-scene_2 = pd.read_csv("/path/to/csv/scene_2.csv", index_col=0)
-scene_3 = pd.read_csv("/path/to/csv/scene_3.csv", index_col=0)
 
-# next step: retrieve rotation & translation from image_auxiliary & apply to X,Y,Z coordinates
+# csv filenames: they are located in /examples for now TODO: dynamically go through the directories
+scenes = ['thres_0382_pointcloud.csv', 'thres_0484_pointcloud.csv']
 
-# first need to convert dataframe to np.ndarray
-scene_1_coord = np.asmatrix(scene_1['X', 'Y', 'Z'].as_matrix())  # should return a np.array
-scene_2_coord = np.asmatrix(scene_2['X', 'Y', 'Z'].as_matrix())
-scene_3_coord = np.asmatrix(scene_3['X', 'Y', 'Z'].as_matrix())
+# Dummy translation vectors, TODO: extract the correct ones from image_auxiliary.csv
+T = [np.array([100, 0, 0]), np.array([0, 100, 0])]
 
-# translation vectors
-T1 = np.array(['x', 'y', 0])  # no translation on z-axis?
-T2 = np.array(['x', 'y', 0])
-T3 = np.array(['x', 'y', 0])
-
-# rotation matrices: should be 3x3, constructed from theta-omega ?
-theta = np.radians(30)
-c, s = np.cos(theta), np.sin(theta)
-R1 = np.matrix([[c, -s, 0], [s, c, 0], [0, 0, 1]])
-R2 = np.matrix([[c, -s, 0], [s, c, 0], [0, 0, 1]])
-R3 = np.matrix([[c, -s, 0], [s, c, 0], [0, 0, 1]])
-
-# apply first rotation to coordinates, then translation
-scene_1_coord = scene_1_coord * R1 + T1
-scene_2_coord = scene_2_coord * R2 + T2
-scene_3_coord = scene_3_coord * R3 + T3
-
-# next step would be to create a vtkActor from the pointcloud array
-scene1_actor = create_pointcloud_actor(scene_1_coord)  # returns a vtk.vtkActor() object
-scene2_actor = create_pointcloud_actor(scene_2_coord)
-scene3_actor = create_pointcloud_actor(scene_3_coord)
+# Rotation matrices: Have to be constructed from theta-omega? TODO: extract the correct ones from image_auxiliary.csv
+R = [np.eye(3), np.eye(3)]
 
 # create renderer object
 renderer = vtk.vtkRenderer()
 renderer.SetBackground(0, 0, 0)
+print 'Renderer created.\n'
 
-# add actors to renderer
-renderer.AddActor(scene1_actor)
-renderer.AddActor(scene2_actor)
-renderer.AddActor(scene3_actor)
+# loop over filenames
+for idx, fn in enumerate(scenes):
+    # read .csv file
+    scene = pd.read_csv(fn, index_col=0)
+    print 'Imported CSV file.\n'
 
-# have no idea what this is
+    # Extract points & colors from the pd.Dataframe
+    points = scene[['X', 'Y', 'Z']].as_matrix()  # should return a np.array
+    colors = scene[['R', 'G', 'B']].as_matrix()
+    print 'Converted pd.Dataframes to np.array.\n'
+
+    # apply first rotation to coordinates, then translation
+    points = np.matmul(points,R[idx]) + T[idx]
+    print "Applied dummy rotation & translation.\n"
+
+    # next step would be to create a vtkActor from the pointcloud array
+    scene_actor = create_pointcloud_actor(points=points, colors=colors)  # returns a vtk.vtkActor() object
+    print "Created Scene Actor.\n"
+
+    # add current scene actor to renderer
+    renderer.AddActor(scene_actor)
+    print "Added Scene Actor to renderer.\n"
+
+    # create camera actor pointing at current scene
+    camera_actor = create_camera_actor(R[idx], T[idx])
+    print "Created Camera Actor.\n"
+    renderer.AddActor(camera_actor)
+    print "Added Camera Actor to renderer.\n"
+
+# create 3D axes representation
 axes = vtk.vtkAxesActor()
 axes.GetXAxisCaptionActor2D().SetHeight(0.05)
 axes.GetYAxisCaptionActor2D().SetHeight(0.05)
 axes.GetZAxisCaptionActor2D().SetHeight(0.05)
 axes.SetCylinderRadius(0.03)
 axes.SetShaftTypeToCylinder()
+# add axes to renderer
 renderer.AddActor(axes)
 
 # create renderer window
 renwin = vtk.vtkRenderWindow()
-renwin.SetWindowName("Point Cloud Viewer")
+renwin.SetWindowName("Point Cloud Lake Plan Viewer")
 renwin.SetSize(800, 600)
 renwin.AddRenderer(renderer)
 
