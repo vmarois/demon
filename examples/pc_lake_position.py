276adf4704fb05688b56714e55b8684414c7973d
vmarois <vincent.marois@protonmail.com>
2018-03-15 16:22:45 +0100

    Handle depth threshold + color. Added todo comments.

--- examples/pc_lake_position.py
+++ examples/pc_lake_position.py
@@ -1,6 +1,7 @@
 import vtk
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 import sys
 
@@ -12,16 +13,26 @@ from depthmotionnet.vis import *
 """
 ** INITIAL TEST **
 - Assume pointclouds have already been exported to CSV with export_pointcloud_to_csv()
-- Assume pointclouds have dropped points towards infinity using find_depth_threshold() & ignore_points_depth_threshold() 
 """
 
-# initial step: load back pointclouds from .csv files
+# csv filenames (entire pointcloud): they are located in /examples for now TODO: dynamically go through the directories
+scenes = ['0382_pointcloud.csv', '0484_pointcloud.csv']
 
-# csv filenames: they are located in /examples for now TODO: dynamically go through the directories
-scenes = ['thres_0382_pointcloud.csv', 'thres_0484_pointcloud.csv']
+# set the path to the dataset TODO: Retrieve the directory from which we took the example '0382.jpg' etc
+DATA_PATH = 'cs-share/pradalier/lake/Dataset/2016/'
 
+#taking image_auxilliary from 'Dataset/2016/160620' as an example for now TODO: How to browse in absolute path?
+image_auxilliary = pd.read_csv('image_auxilliary.csv')
+print "Correctly loaded image_auxilliary as pd.Dataframe"
+print image_auxilliary.head()
+"""
+TODO next:
+Construct the filepath using 'seq' to get access to the source images (depends on the directory structure)
+For a given image, extract x, y, theta, pan, tilt to construct translation vector & rotation matrix
+Apply these to the pointcloud and check display
+"""
 # Dummy translation vectors, TODO: extract the correct ones from image_auxiliary.csv
-T = [np.array([100, 0, 0]), np.array([0, 100, 0])]
+T = [np.array([10, 0, 0]), np.array([0, 10, 0])]
 
 # Rotation matrices: Have to be constructed from theta-omega? TODO: extract the correct ones from image_auxiliary.csv
 R = [np.eye(3), np.eye(3)]
@@ -33,9 +44,18 @@ print 'Renderer created.\n'
 
 # loop over filenames
 for idx, fn in enumerate(scenes):
-    # read .csv file
+    # read .csv file containing the entire pointcloud
     scene = pd.read_csv(fn, index_col=0)
     print 'Imported CSV file.\n'
+
+    # find the depth threshold
+    n, bins, patches = plt.hist(scene['Z'], bins=40, normed=True)  # extract info from the histogram
+    threshold = np.ceil(bins[np.argmax(n) + 1])  # select the bin directly on the right of the argmax bin
+    print '\nDepth threshold : ', threshold
+
+    # keep only points with Z < threshold
+    scene = scene.ix[scene['Z'] <= threshold]
+    print "Removed points with Z > depth threshold"
 
     # Extract points & colors from the pd.Dataframe
     points = scene[['X', 'Y', 'Z']].as_matrix()  # should return a np.array
